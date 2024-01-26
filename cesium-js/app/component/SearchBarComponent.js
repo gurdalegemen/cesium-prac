6b511f55aaa785eb6c7e846d263147b1acd2001b
EGEMEN GURDAL UYAN <21796221@ogr.baskent.edu.tr>
2024-01-26 14:36:46 +0300

    Searchbar done

--- cesium-js/app/component/SearchBarComponent.js
+++ cesium-js/app/component/SearchBarComponent.js
@@ -1,12 +1,25 @@
-import { Input, Button, Icon, Search } from "semantic-ui-react"
+import { Input, Button, ButtonGroup, Icon } from "semantic-ui-react"
 
 export default function SearchBarComponent(){
     return(
-        <div style={{display:'flex', alignItems:'center', borderRadius:'10px', padding:'2px'}}>
-            <Input id="searchInput" icon="red map marker alternate" iconPosition="left" action={{ icon: 'search' }} placeholder="Mekan ve adres arama" size="large"/>
-            <div style={{}}>
-                <Button id="searchButton" icon="caret right" type="submit"/>
+        <>
+        <div style={{display:'flex', alignItems:'center', padding:'2px'}}>
+            <div style={{display:'flex', width:'358px'}}>
+                <Icon id="searchMarker" name="map marker alternate" color="red" size="large"/>
+                <Input id="searchInput" placeholder="Mekan ve adres arama" size="large"/>
+                <ButtonGroup id="searchCustomBtnGroup" basic>
+                    <Button id="searchInputButton">
+                        <Icon id="iconMarginPattern" name="search" size="large"/>
+                    </Button>
+                    <Button id="searchRouteButton">
+                        <Icon id="iconMarginPattern" name="map signs" size="large"/>
+                    </Button>
+                </ButtonGroup>
             </div>
         </div>
+        <div style={{position:'absolute', top:0, marginTop:'16px', marginLeft:'372px'}}>
+            <Button id="searchButton" icon="caret right" type="submit"/>
+        </div>
+        </>
     )
 }
