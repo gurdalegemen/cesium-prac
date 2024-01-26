18a044142525fe72361e72e08013035fd1b7d421
EGEMEN GURDAL UYAN <21796221@ogr.baskent.edu.tr>
2024-01-26 11:06:46 +0300

    Map Control Component done

--- cesium-js/app/component/MapControlComponent.js
+++ cesium-js/app/component/MapControlComponent.js
@@ -3,17 +3,25 @@ import { ButtonGroup,Button, Icon } from "semantic-ui-react";
 export default function MapControlComponent(){
     return(
         <div style={{display:'flex', flexDirection:'column', alignItems:'center',justifyContent:'flex-end', margin:'0'}}>
-            <div style={{backgroundColor:'white', borderRadius:'12px', width:'40px'}}>
-                <ButtonGroup basic vertical>
+            <div style={{borderRadius:'12px',height:'80px', width:'40px', display:'flex', alignItems:'center'}}>
+                <ButtonGroup id="customButtonGroup" basic vertical>
                     <Button id="customButton" icon="plus"/>
                     <Button id="customButton" icon="minus"/>
                 </ButtonGroup>
             </div>
-            <div style={{backgroundColor:'white', borderRadius:'12px', height:'40px', width:'40px', display:'flex', alignItems:'center',justifyContent:'center', padding:'4px', marginTop:'8px'}}>
-                <Button id="locationCustomButton" icon="location arrow"/>
+            <div style={{padding:'4px', marginTop:'8px'}}>
+                <div style={{backgroundColor:'white', borderRadius:'12px', height:'40px', width:'40px', display:'flex', alignItems:'center',justifyContent:'center'}}>
+                    <Button id="locationCustomButton">
+                        <Icon name="location arrow" size="large" color="gray"/>
+                    </Button>
+                </div>
             </div>
-            <div style={{backgroundColor:'white', borderRadius:'12px', color:'grey',display:'flex', alignItems:'center',justifyContent:'center', height:'40px', width:'40px', marginTop:'8px'}}>
-                <Icon id="compassIcon" name="compass outline" size="big"/>
+            <div style={{marginTop:'8px'}}>
+                <div style={{backgroundColor:'white', borderRadius:'12px', color:'grey',display:'flex', alignItems:'center',justifyContent:'center', height:'40px', width:'40px'}}>
+                    <Button id="compassCustomButton">
+                        <Icon name="compass" size="big" color="gray"/>
+                    </Button>
+                </div>
             </div>
         </div>
     )
--- cesium-js/app/component/ToolbarComponent.js
+++ cesium-js/app/component/ToolbarComponent.js
@@ -5,34 +5,34 @@ export default function ToolbarComponent(){
     return(
         <>
  
-                <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', height:'40px', alignItems:'center'}}>
-                    <div style={{padding:'8px'}}>
-                        <Icon fitted name="road" size="large" color="grey"/>
+                <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', height:'40px', alignItems:'center', height:'40px', width:'200px'}}>
+                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
+                        <Icon className="toolbarIcons" fitted name="road" size="large" color="grey"/>
                     </div>
-                    <div style={{padding:'8px'}}>
-                        <Icon fitted name="search plus" size="large" color="grey"/>
+                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
+                        <Icon className="toolbarIcons" fitted name="search plus" size="large" color="grey"/>
                     </div>
-                    <div style={{padding:'8px'}}>
-                        <Icon fitted name="camera" size="large" color="grey"/>
+                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
+                        <Icon className="toolbarIcons" fitted name="camera" size="large" color="grey"/>
                     </div>
-                    <div style={{padding:'8px'}}>
-                        <Icon fitted name="bus" size="large" color="grey"/>
+                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
+                        <Icon className="toolbarIcons" fitted name="bus" size="large" color="grey"/>
                     </div>
-                    <div style={{padding:'8px'}}>
-                        <Icon fitted name="clone outline" size="large" color="grey"/>
-                    </div>
-                </div>
-                <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', marginLeft:'12px'}}>
-                    <div style={{padding:'8px'}}>
-                        <Icon fitted name="edit outline" size="large" color="grey"/>
-                    </div>
-                    <div style={{padding:'8px'}}>
-                        <Icon fitted name="ellipsis horizontal" size="large" color="grey"/>
+                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
+                        <Icon className="toolbarIcons" fitted name="clone outline" size="large" color="grey"/>
                     </div>
                 </div>
-                <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', marginLeft:'12px'}}>
-                    <div style={{padding:'8px'}}>
-                        <Icon fitted name="bars" size="large" color="grey"/>
+                <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', marginLeft:'12px', height:'40px', width:'80px', alignItems:'center'}}>
+                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
+                        <Icon className="toolbarIcons" fitted name="edit outline" size="large" color="grey"/>
+                    </div>
+                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
+                        <Icon className="toolbarIcons" fitted name="ellipsis horizontal" size="large" color="grey"/>
+                    </div>
+                </div>
+                <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', marginLeft:'12px', height:'40px', width:'40px', alignItems:'center'}}>
+                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
+                        <Icon className="toolbarIcons" fitted name="bars" size="large" color="grey"/>
                     </div>
                 </div>
         </>
