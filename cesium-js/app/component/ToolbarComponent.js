3d844986f623de96d5fdc60f9356af097a4f1e8d
EGEMEN GURDAL UYAN <21796221@ogr.baskent.edu.tr>
2024-01-29 09:52:01 +0300

    yandex clone with no functionality

--- cesium-js/app/component/ToolbarComponent.js
+++ cesium-js/app/component/ToolbarComponent.js
@@ -1,4 +1,4 @@
-import { Icon } from "semantic-ui-react";
+import { Button, ButtonGroup, Icon } from "semantic-ui-react";
 
 
 export default function ToolbarComponent(){
@@ -6,34 +6,43 @@ export default function ToolbarComponent(){
         <>
  
                 <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', height:'40px', alignItems:'center', height:'40px', width:'200px'}}>
-                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
-                        <Icon className="toolbarIcons" fitted name="road" size="large" color="grey"/>
-                    </div>
-                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
-                        <Icon className="toolbarIcons" fitted name="search plus" size="large" color="grey"/>
-                    </div>
-                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
-                        <Icon className="toolbarIcons" fitted name="camera" size="large" color="grey"/>
-                    </div>
-                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
-                        <Icon className="toolbarIcons" fitted name="bus" size="large" color="grey"/>
-                    </div>
-                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
-                        <Icon className="toolbarIcons" fitted name="clone outline" size="large" color="grey"/>
-                    </div>
+                    <ButtonGroup basic id="toolbarButtonGroup">
+                        <Button id="toolbarButtons">
+                            <Icon name="road" size="large" color="black"/>
+                        </Button>
+                        <Button id="toolbarButtons">
+                            <Icon name="search plus" size="large" color="black"/>
+                        </Button>
+                        <Button id="toolbarButtons">
+                            <Icon  name="camera" size="large" color="black"/>
+                        </Button>
+                        <Button id="toolbarButtons">
+                            <Icon name="bus" size="large" color="black"/>
+                        </Button>
+                        <Button id="toolbarButtons">
+                            <Icon name="clone outline" size="large" color="black"/>
+                        </Button>
+                    </ButtonGroup>
                 </div>
-                <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', marginLeft:'12px', height:'40px', width:'80px', alignItems:'center'}}>
-                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
-                        <Icon className="toolbarIcons" fitted name="edit outline" size="large" color="grey"/>
-                    </div>
-                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
-                        <Icon className="toolbarIcons" fitted name="ellipsis horizontal" size="large" color="grey"/>
-                    </div>
+                <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', marginLeft:'12px', height:'40px', width:'80px', alignItems:'center',justifyContent:'center', border:'none'}}>
+                    <ButtonGroup basic id="toolbarButtonGroup">
+                        <Button id="toolbarButtons">
+                            <Icon name="edit outline" size="large" color="black"/>
+                        </Button>
+                        <Button id="toolbarButtons">
+                            <Icon name="ellipsis horizontal" size="large" color="black"/>     
+                        </Button>
+                    </ButtonGroup>
                 </div>
                 <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', marginLeft:'12px', height:'40px', width:'40px', alignItems:'center'}}>
-                    <div style={{padding:'8px', height:'40px', width:'40px'}}>
+                        <ButtonGroup basic id="toolbarButtonGroup">
+                            <Button id="toolbarButtons">
+                                <Icon name="bars" size="large" color="black"/>     
+                            </Button>
+                        </ButtonGroup>
+                    {/* <div style={{padding:'8px', height:'40px', width:'40px'}}>
                         <Icon className="toolbarIcons" fitted name="bars" size="large" color="grey"/>
-                    </div>
+                    </div> */}
                 </div>
         </>
 
