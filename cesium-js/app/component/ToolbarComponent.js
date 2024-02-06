fc2c465ff9f11447fd868b45ecba61e80014b382
EGEMEN GURDAL UYAN <21796221@ogr.baskent.edu.tr>
2024-02-06 17:48:05 +0300

    dropdown fixing

--- cesium-js/app/component/ToolbarComponent.js
+++ cesium-js/app/component/ToolbarComponent.js
@@ -1,14 +1,15 @@
-import { SceneMode, ImageryLayer, OpenStreetMapImageryProvider, BingMapsImageryProvider, BingMapsStyle } from "cesium";
+import { BingMapsImageryProvider, BingMapsStyle } from "cesium";
 import { useState } from "react";
-import { Button, ButtonGroup, Icon, Dropdown } from "semantic-ui-react";
+import { Button, ButtonGroup, Icon, Dropdown, DropdownMenu, DropdownItem } from "semantic-ui-react";
 
 
 export default function ToolbarComponent(view){
-    const [selectedMode, setSelectedMode] = useState('3D');
+    
+    const [selectedMode, setSelectedMode] = useState('');
 
     const handleModeChange = async (event, { value }) => {
       setSelectedMode(value);
-      if(value === "Satelite"){
+      if(value === "Satellite"){
         const bingLabelMap = await BingMapsImageryProvider.fromUrl("http://dev.virtualearth.net",{
             key: process.env.NEXT_PUBLIC_BING_MAPS_API_KEY, // Replace with your Bing Maps API key
             mapStyle: BingMapsStyle.AERIAL,
@@ -30,14 +31,16 @@ export default function ToolbarComponent(view){
             key: process.env.NEXT_PUBLIC_BING_MAPS_API_KEY, // Replace with your Bing Maps API key
             mapStyle: BingMapsStyle.AERIAL_WITH_LABELS,
         })
-        view.viewer.scene.imageryLayers.addImageryProvider(bingLabelMap);
+        view.viewer.scene.imageryLayers.addImageryProvider(bingLabelMap);   
           
       }
       // You can add additional logic here based on the selected mode
+
+    
     };
   
     const options = [
-      { key: 'Satelite', text: 'Satelite', value: 'Satelite' },
+      { key: 'Satellite', text: 'Satellite', value: 'Satellite' },
       { key: 'Roadmap', text: 'Roadmap', value: 'Roadmap' },
       { key: 'Hybrid', text: 'Hybrid', value: 'Hybrid' },
     ];
@@ -64,15 +67,12 @@ export default function ToolbarComponent(view){
                           button
                           className='icon'
                           floating
-                        //   labeled
                           icon={<Icon name="clone outline" size="large" color="black"/>}
+                          lazyLoad
                           options={options}
                           onChange={handleModeChange}
                           value={selectedMode}
                         />
-                        {/* <Button id="toolbarButtons">
-                            <Icon name="clone outline" size="large" color="black"/>
-                        </Button> */}
                     </ButtonGroup>
                 </div>
                 <div style={{display:'flex',borderRadius:'12px', backgroundColor:'white', marginLeft:'12px', height:'40px', width:'80px', alignItems:'center',justifyContent:'center', border:'none'}}>
